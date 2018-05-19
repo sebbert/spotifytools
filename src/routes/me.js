9687bc3e9481eabcb37a3191175377e15b396873
Sebastian Reinhard <sebastian@fusetools.com>
2018-05-19 21:55:02 +0200

    Pass user object to templates under /me

--- src/routes/me.js
+++ src/routes/me.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const {ensureLoggedIn} = require("connect-ensure-login");
+const renderWithContext = require("../renderWithContext");
 
 router.use(ensureLoggedIn("/"));
+
+router.use(
+	renderWithContext((req, res) => {
+		return {
+			user: req.user
+		}
+	})
+)
 
 router.get("/", (req, res) => {
 	res.render("me/index");
