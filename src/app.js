eae1c85fb2d3624b1a3895a607bb7be3e21f5da8
Sebastian Reinhard <sebastian@fusetools.com>
2018-05-19 15:37:44 +0200

    Serve files from src/static under /static

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
+const path = require("path");
 
 app.set("views", "src/views");
 app.set("view engine", "pug");
+
+const staticPath = path.join(__dirname, "static");
+app.use("/static", express.static(staticPath));
 
 app.use(routes);
 
