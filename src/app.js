c7e25bfe924c7b5574d550b4d4f7ab6856b45df4
Sebastian Reinhard <sebastian@fusetools.com>
2018-05-19 15:33:53 +0200

    Set up express views with pug

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 
+app.set("views", "src/views");
+app.set("view engine", "pug");
+
 app.use(routes);
 
 module.exports = app;
