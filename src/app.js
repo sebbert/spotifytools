0e0c68d3cdc454d32cf8ae995cc3babb1a41675e
Sebastian Reinhard <sebastian@fusetools.com>
2018-05-19 16:21:07 +0200

    Add morgan for logging

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const path = require("path");
+const morgan = require("morgan");
 
 app.set("views", "src/views");
 app.set("view engine", "pug");
+
+app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"))
 
 const staticPath = path.join(__dirname, "static");
 app.use("/static", express.static(staticPath));
