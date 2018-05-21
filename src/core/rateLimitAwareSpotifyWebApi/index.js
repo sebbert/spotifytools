de17950d93e6b1d810cb50b00a590c7b76b98b08
Sebastian Reinhard <sebastian@fusetools.com>
2018-05-21 22:33:41 +0200

    implement RateLimitAwareSpotifyWebApi

--- src/core/rateLimitAwareSpotifyWebApi/index.js
+++ src/core/rateLimitAwareSpotifyWebApi/index.js
@@ -1,6 +1,18 @@
 // This entire thing is a huge hack, should fork the spotify-web-api-node library to support this, but oh well.
 
-const WebApiRequest = require("spotify-web-api-node/src/webapi-request");
-const BaseRequest = require("spotify-web-api-node/src/base-request");
+const SpotifyWebApi = require("spotify-web-api-node");
+const retryIfRateLimited = require("./retryIfRateLimited");
 
 require("./patchSpotifyWebApi");
+
+class RateLimitAwareSpotifyWebApi extends SpotifyWebApi {}
+
+for(let key in SpotifyWebApi.prototype) {
+	const method = SpotifyWebApi.prototype[key];
+	RateLimitAwareSpotifyWebApi.prototype[key] = function(...args) {
+
+		return retryIfRateLimited(() => method.apply(this, args))
+	}
+}
+
+module.exports = RateLimitAwareSpotifyWebApi;
