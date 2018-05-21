8b6c1dba7a97fdb6b332abe875132aae551c8d7b
Sebastian Reinhard <sebastian@fusetools.com>
2018-05-21 15:04:53 +0200

    Add db model for managed playlists

--- src/models.js
+++ src/models.js
@@ -1,6 +1,7 @@
 const mg = require("mongoose");
 const T = mg.Schema.Types;
 
+
 const UserSchema = mg.Schema({
 	spotifyId: T.String,
 	spotifyProfile: T.Mixed,
@@ -13,7 +14,20 @@ const UserSchema = mg.Schema({
 
 const User = mg.model("User", UserSchema);
 
+const ManagedPlaylistSchema = mg.Schema({
+	user: T.ObjectId,
+	sourcePlaylistName: T.String,
+	sourcePlaylistId: T.String,
+	managedPlaylistName: T.String,
+	managedPlaylistId: T.String,
+	lastSnapshotId: T.String,
+});
+
+const ManagedPlaylist = mg.model("ManagedPlaylist", ManagedPlaylistSchema);
+
 module.exports = {
 	User,
 	UserSchema,
+	ManagedPlaylist,
+	ManagedPlaylistSchema,
 };
